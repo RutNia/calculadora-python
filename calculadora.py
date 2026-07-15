2b541ae6b0e4717611534258e6f203d599103ef9
RutNia <ruth.benitez.98@est.ucacue.edu.ec>
2026-07-14 23:04:40 -0500

    v1.0 - Creación inicial del archivo y estructura del proyecto

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+"""
+Calculadora en Python
+Proyecto de práctica para el deber de Control de Versiones.
+Responsable del proyecto: Ruth Benítez (Líder de Proyecto)
+"""
+
+def main():
+    print("Calculadora en construcción...")
+
+if __name__ == "__main__":
+    main()
